ab93a4e2462e53c00f2f3d3b38b9e29900c575f9
Sohail <thegrabsinfo@gmail.com>
2020-05-10 02:28:28 +0500

    'endpoint'

--- push.js
+++ push.js
@@ -7,6 +7,6 @@ let vapidKeys = {
 
 push.setVapidDetails('mailto:test@localsite.com',vapidKeys.publicKey,vapidKeys.privateKey)
 
-let sub = {};
+let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/e6jVnjd-zRY:APA91bG1oHd3GCRf4bpwcnIvKa5n4WDMWt6ZVLxZ8K8OQE1WhB_qPBWtPlk6_jPeHyzItYZq86gztSAMNzBRwGwK9Gq280Kh6qr4YOE7-H1NCNSwDIy22flL0XGvTPC1Q4XHE4ANWu6S","expirationTime":null,"keys":{"p256dh":"BNiFKWDovjGoALi3dI4keozmdwQpS4ubloZ0ylfsyAz8UmX-7XojRHp0zBL8Cwl2wwUFFfpj1bZAz_iuuqi4jWY","auth":"Kc1H-XeY-rQB_42qMwOJow"}}
 
 push.sendNotifcation(sub, 'Test Message')
