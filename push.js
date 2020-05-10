4d481c86f812192d6ed2fd976490ffe182958af3
Sohail <thegrabsinfo@gmail.com>
2020-05-10 20:17:03 +0500

    push is now php

--- push.js
+++ push.js
@@ -7,6 +7,6 @@ let vapidKeys = {
 
 push.setVapidDetails('mailto:test@localsite.com',vapidKeys.publicKey,vapidKeys.privateKey)
 
-let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/fSWsdxpPD4w:APA91bFILRxzkNbo116r9hy8xJeDNYMCexD2d-EXL_vyO06rUw5P2iuDWSkJR-xFUzAP1uMv-I_8zw4iY6yJw7qZbf57pP3PyiLzFi4hI3mtWrU9sdR2OdoQyaLbjDJ_TE5GTJNA9QjS","expirationTime":null,"keys":{"p256dh":"BJznKbgnIxdqiE6QwsXtJNG2Te36zTPOuiS6KQXCgs9kXqJJIFR23XlNb4DIrUMYKjkFnMGBDjIJH0qzHxiV9_0","auth":"USAa2hbnySh-Vt5Y3ET3BA"}}
+let sub = {"endpoint":"https://updates.push.services.mozilla.com/wpush/v2/gAAAAABetyLipXwPvvVp-xvws5KwtlhsR7wU7VPrrNYEvbVuVHd1t7h9K4tj-D1nhaEPSJugMB-X_sVeoqB0raAkf3SrRqxpznKNr-0KNsyHKgG1XVwdtKdVeD4KqHmx_uw9epYeqJfUJExzxUlfIh3cDyX6euZaGgK7z9WVppmvikofVBOvXNQ","keys":{"auth":"WKlWG1__GLnbCf9hqWhVRg","p256dh":"BMUw-bfl2mfD-lwkK5SwpKdCtI8u5DVw7BHEuK3zKwitgum5mPMgyznof7sz6KVrLRYUeRFzu1y6fg2d7MfKIhI"}}
 
 push.sendNotification(sub, 'Test Message');
